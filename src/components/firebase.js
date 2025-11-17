2c1bb73447e486d6b7610e45e3c16fd5c4bdf97d
alosantader <alosantamex@gmail.com>
2025-11-17 17:49:34 -0600

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://sqhtdjsxtwmcyznmjjjv.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxaHRkanN4dHdtY3l6bm1qamp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5MTg1OTcsImV4cCI6MjA1MjQ5NDU5N30.xQ97CFSEcOjNcjZKTM5rxuLLxtptEt8SzgBMmDR3X_0';
+const SUPABASE_URL = 'https://rjvvfqgriglasqzporat.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqdnZmcWdyaWdsYXNxenBvcmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODU2MzksImV4cCI6MjA3ODk2MTYzOX0.btAeQt5juu6L7Mzas6EM9c4JFc4sGP6WTbqxbBKrlp0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
